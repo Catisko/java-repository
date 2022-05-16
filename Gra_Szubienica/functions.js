a013ac26c19d6e9f5de395b217b4e24ed85d9259
wojtek urbanek <Wojtek>
2022-05-16 16:45:34 +0200

    Update 16.05

--- Gra_Szubienica/functions.js
+++ Gra_Szubienica/functions.js
@@ -1,12 +1,7 @@
-
-
-
-//ok
 const chooseRandomWord = function (tableOfWords) {  //1. 3,  2. 4. 5.
     return tableOfWords[Math.floor(Math.random() * tableOfWords.length)]
 }
 
-//ok 
 const prepareTableOfAnswers = function (word) {  //1. 3 2. 4. 5.
     let answers = []
     for (let i = 0; i < word.length; i++) {  //1. 3 2. 4. 5.
@@ -33,21 +28,15 @@ const whichLettersWereUsed = function (shot, arrayOfLetters) {
     return { arrayOfLetters, foundLettersUsedTwice }
 }
 
-
-//ok
 const showCurrentScore = function (answers, arrayOfLetters) {  //1. 3. 2. 5.
     alert("Bieżący stan gry " + answers.join(" ") + " , użyte dotychczas literki to: " + arrayOfLetters.join(","))
 }
 
-//ok
 const loadPlayerShot = function (steps, shot) {  //1. 3 2. 5.
     shot = prompt("podaj literę")
     steps = steps + 1
     return shot
 }
-
-
-//ok
 const updateGameState = function (shot, randomWord, answers, leftLetters, usersLifeNumber) {  //1. 3  5.
     let leftLettersTemp = leftLetters
     for (let i = 0; i < randomWord.length; i++) {  //1. 3  5.
@@ -67,12 +56,10 @@ const updateGameState = function (shot, randomWord, answers, leftLetters, usersL
     return { answers, leftLetters, usersLifeNumber }  //1. 3 
 }
 
-//ok
 const showAnswersAndCongratulate = function (randomWord) {  //1.
     alert("Dobra robota, szukane słowo to " + randomWord)
 }
 
-//ok
 const initialStateOfLife = function () {  //1. 3  4.
     let usersLifeNumber = prompt("Podaj ilość żyć")
     if (usersLifeNumber === null) {  //1. 3. 
@@ -82,52 +69,21 @@ const initialStateOfLife = function () {  //1. 3  4.
     return usersLifeNumber  //1. 3 
 }
 
-//ok
-const checkEndGameCondition = function (usersLifeNumber, leftLetters, randomWord, steps) {  //1. 3 
-    if (usersLifeNumber <= 0) { //3 
-        alert("Skończyły ci się życia! Dziekuje za gre. Szukane słowo to " + randomWord)
+const checkEndGameCondition = function (usersLifeNumber, leftLetters, randomWord, steps, ifUsedInTest) {  //1. 3 
+    if (usersLifeNumber <= 0) {
+        if (ifUsedInTest == false) {
+            alert("Skończyły ci się życia! Dziekuje za gre. Szukane słowo to " + randomWord)
+        }
         return true //3 
-    } else if (leftLetters === 0) {  //1.
-        alert("odgadles haslo w tylu krokach:  " + steps)
-        alert("Dobra robota, szukane słowo to " + randomWord)  //1.
+    } else if (leftLetters === 0) {
+        if (ifUsedInTest == false) {
+            alert("odgadles haslo w tylu krokach:  " + steps)
+            alert("Dobra robota, szukane słowo to " + randomWord)
+        } //1.
         return true  //1.
     } else { //1. //3.
         return false //1. 3. 
     }
 }
 
-// 1. testuje usersLifeNumber = 0 leftLetters = 1 sprawdzenie czy zwróci true 
-// 2. testuje usersLifeNumber = 1 leftLetters = 0 sprawdzenie czy zwróci true
-// 3. testuje usersLifeNumber = 1 leftLetters =  sprawdzemie czy zwróci false 
-
-
-
-
-
-
-
-
-
-// dodanie ilości żyć domyślnie i z opcją wyboru ilości przez gracza. (komunikat o utracie żyć)
-// co strzał informuje o trafieniu true/false błędna odp = -1 życie (wyświetla komunikat o końcu żyć i kończy oraz podaje hasło)
-// informuje o tym czy literka została użyta ponownie, zapamiętuje już użyte literki (wystawia komunikat)
-// liczy kroki w ilu gracz odgadł hasło(wystawia komunikat)
-
-
-
-
-//tutaj zapytaj o zycia i przypisz do jakiejs zmiennej np usersLifeNumber
-
-
-
-
-
-
-
-
-
-export const testowanie = { prepareTableOfAnswers, checkEndGameCondition, whichLettersWereUsed }
 export const functions = { chooseRandomWord, prepareTableOfAnswers, whichLettersWereUsed, showCurrentScore, loadPlayerShot, updateGameState, showAnswersAndCongratulate, initialStateOfLife, checkEndGameCondition }
-
-
-
--- Gra_Szubienica/tests_szubienica.js
+++ Gra_Szubienica/tests_szubienica.js
@@ -1,8 +1,4 @@
-import { testowanie } from "./functions"
-
-
-
-//TESTY
+import { functions } from './functions.js'
 
 // 1. odgadniecie wszystkich liter hasła bez pomyłki i zakonczenie gry (caly czas poprawne litery)
 // 2. nacisniecie przycisku anuluj konczy gre (po dodaniu zyc przez uzytkownika nacisnij anuluj)
@@ -10,13 +6,9 @@ import { testowanie } from "./functions"
 // 4. wyłączenie strony z grą
 // 5. podawanie caly czas liczby liter wiekszej od 1 (w polu podaj litere)
 
-//ok
-
-
-
 const checkPrepareTableOfAnswers = function () {
-    var word = "monitor"
-    var preparedAnswer = testowanie.prepareTableOfAnswers(word)
+    let word = "monitor"
+    let preparedAnswer = functions.prepareTableOfAnswers(word)
 
     if (preparedAnswer.length === word.length) {
         alert("przygotowane odpowiedzi maja poprawna dlugosc")
@@ -24,7 +16,7 @@ const checkPrepareTableOfAnswers = function () {
         alert("przygotowane odpowiedzi nie maja poprawnej dlugosci")
     }
 
-    var isDifferent = false
+    let isDifferent = false
     for (let i = 0; i < preparedAnswer.length; i++) {
         if (preparedAnswer[i] != "_") {
             isDifferent = true
@@ -38,56 +30,49 @@ const checkPrepareTableOfAnswers = function () {
     }
 }
 
-// checkPrepareTableOfAnswers()
-
-
+// 1. testuje usersLifeNumber = 0 leftLetters = 1 sprawdzenie czy zwróci true 
+// 2. testuje usersLifeNumber = 1 leftLetters = 0 sprawdzenie czy zwróci true
+// 3. testuje usersLifeNumber = 1 leftLetters = 1 sprawdzemie czy zwróci false 
 
 const checkUsersLifeNumber = function () {
-    // 1. testuje usersLifeNumber = 0 leftLetters = 1 sprawdzenie czy zwróci true 
-    // 2. testuje usersLifeNumber = 1 leftLetters = 0 sprawdzenie czy zwróci true
-    // 3. testuje usersLifeNumber = 1 leftLetters = 1 sprawdzemie czy zwróci false 
-    var testLeftLetters = 0
-    var testUserLifeNumber = 1
-    var preparedUserLifeNumber = testowanie.checkEndGameCondition(testUserLifeNumber, testLeftLetters)
+    let testLeftLetters = 0
+    let testUserLifeNumber = 1
+    let ifUsedInTest = true
+    let preparedUserLifeNumber = functions.checkEndGameCondition(testUserLifeNumber, testLeftLetters, ifUsedInTest)
     if (preparedUserLifeNumber == true) {
         alert("funkcja działa dobrze 1")
     } else {
         alert("Funkcja nie działa dobrze")
     }
-    var testLeftLetters = 1
-    var testUserLifeNumber = 0
-    preparedUserLifeNumber = testowanie.checkEndGameCondition(testUserLifeNumber, testLeftLetters)
+    testLeftLetters = 1
+    testUserLifeNumber = 0
+    preparedUserLifeNumber = functions.checkEndGameCondition(testUserLifeNumber, testLeftLetters, ifUsedInTest)
     if (preparedUserLifeNumber == true) {
         alert("funkcja działa dobrze 2")
     } else {
         alert("Funkcja nie działa dobrze")
     }
-    var testLeftLetters = 1
-    var testUserLifeNumber = 1
-    preparedUserLifeNumber = testowanie.checkEndGameCondition(testUserLifeNumber, testLeftLetters)
+    testLeftLetters = 1
+    testUserLifeNumber = 1
+    preparedUserLifeNumber = functions.checkEndGameCondition(testUserLifeNumber, testLeftLetters, ifUsedInTest)
     if (preparedUserLifeNumber == false) {
         alert("funkcja działa dobrze 3")
     } else {
         alert("Funkcja nie działa dobrze")
     }
-
 }
-
-// checkUsersLifeNumber()
-
-
 
 // 1. test shot = 'a' arrayOfLetters = [] arrayOfLetters= ['a'], foundLettersUsedTwice=false
 // 2. test shot = 'b'  arrayOfLetters = ['a' 'b'] arrayOfLetters= ['a' 'b'],  foundLettersUsedTwice=true
 // 3. test shot = 'c'  arrayOfLetters = ['a' 'b']  arrayOfLetters = ['a' 'b' 'c'], foundLettersUsedTwice=false
 
 const checkWhichLettersWereUsed = function () {
-    var testShot = "A"
-    var testArrayOfLetters = []
-    var arrayOfLetters
-    var foundLettersUsedTwice;
+    let testShot = "A"
+    let testArrayOfLetters = []
+    let arrayOfLetters
+    let foundLettersUsedTwice;
 
-    ({ arrayOfLetters, foundLettersUsedTwice } = testowanie.whichLettersWereUsed(testShot, testArrayOfLetters))
+    ({ arrayOfLetters, foundLettersUsedTwice } = functions.whichLettersWereUsed(testShot, testArrayOfLetters))
     if (foundLettersUsedTwice == false) {
         alert("funkcja działa prawidłowo 1")
     } else {
@@ -106,15 +91,9 @@ const checkWhichLettersWereUsed = function () {
         alert("Literka nie znajduje sie na tym miejscu")
     }
 
-
-
-    // Poznanie i wykonywanie testów białoskrzynkowych w prostej grze.
-
-    //////////////// liczy długość i pokazuje na ktorym jest miejscu
-
     testShot = "b"
     testArrayOfLetters = ["a", "b"];
-    ({ arrayOfLetters, foundLettersUsedTwice } = testowanie.whichLettersWereUsed(testShot, testArrayOfLetters))
+    ({ arrayOfLetters, foundLettersUsedTwice } = functions.whichLettersWereUsed(testShot, testArrayOfLetters))
     if (foundLettersUsedTwice == true) {
         alert("funkcja działa prawidłowo 2")
     } else {
@@ -133,10 +112,9 @@ const checkWhichLettersWereUsed = function () {
         alert("Literka nie znajduje sie na tym miejscu")
     }
 
-
     testShot = "c"
     testArrayOfLetters = ["a", "b"];
-    ({ arrayOfLetters, foundLettersUsedTwice } = testowanie.whichLettersWereUsed(testShot, testArrayOfLetters))
+    ({ arrayOfLetters, foundLettersUsedTwice } = functions.whichLettersWereUsed(testShot, testArrayOfLetters))
     if (foundLettersUsedTwice == false) {
         alert("funkcja działa prawidłowo 3")
     } else {
@@ -154,11 +132,6 @@ const checkWhichLettersWereUsed = function () {
     } else {
         alert("Literka nie znajduje sie na tym miejscu")
     }
-
 }
 
-export const testy = { checkPrepareTableOfAnswers, checkUsersLifeNumber, checkWhichLettersWereUsed }
-
-
-
-
+export const tests = { checkPrepareTableOfAnswers, checkUsersLifeNumber, checkWhichLettersWereUsed }
